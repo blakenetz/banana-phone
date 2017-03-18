6a2d9ef4c5357afaf52a801fe040c98b26f16022
Blake Netzeband <blake.netzeband@gmail.com>
2017-03-18 12:55:03 -0600

    more verbose

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -22,7 +22,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      loader: 'babel',
+      loader: 'babel-loader',
       exclude: /node_modules/,
       query: {
         presets: ['react', 'es2015', 'stage-1']
