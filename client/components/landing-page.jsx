0aadf83dd558301ac219e3fa58fd834e17dcb723
Blake Netzeband <blake.netzeband@gmail.com>
2017-03-18 11:32:51 -0600

    landing page

---
+++ client/components/landing-page.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class LandingPage extends Component {
+  render(){
+    return (
+      <section className="lp">
+        <img src="images/logo.png" />
+        <h1>A Phone With Appeal</h1>
+      </section>
+    )
+  }
+
+}
+
+export default LandingPage;
