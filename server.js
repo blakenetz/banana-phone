43be528feeb7393551722acd0d6a214a3fd6a14f
Blake Netzeband <blake.netzeband@gmail.com>
2017-03-19 19:30:40 -0600

    node favicon file path update

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use(favicon(path.join(__dirname, 'images/favicons', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'dist/images/favicons', 'favicon.ico')));
 app.set('views', path.join(__dirname, 'dist'));
 
 app.post('/signup', (req, res) => {
