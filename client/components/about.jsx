63664fe716581eb1e8745086259731f14388e44a
Blake Netzeband <blake.netzeband@gmail.com>
2017-03-19 18:28:07 -0600

    finished form! added a few media queries

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -1,10 +1,116 @@
 import React, { Component } from 'react';
 
 class About extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      formSent: false,
+      invalidEmail: false,
+      reqName: false,
+      reqEmail: false,
+      name: '',
+      email: '',
+      submitButton: 'SUBMIT'
+    }
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e){
+    const name = e.target.name
+    const value = e.target.value
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleBlur(e){
+    let name = e.target.name
+    let value = e.target.value
+
+    switch (name){
+      case 'name':
+        value.trim().length > 0 ? this.setState({ reqName: false }) : this.setState({ reqName: true });
+        break;
+      case 'email':
+        const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        reg.test(value) ? this.setState({ invalidEmail: false }) : this.setState({ invalidEmail: true })
+        value.trim().length > 0 ? this.setState({ reqEmail: false }) : this.setState({ reqEmail: true });
+        break
+    }
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+
+    fetch('http://localhost:3000/signup', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        email: this.state.email
+      })
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        if (responseJson.success) {
+          this.setState({formSent: true})
+          this.setState({submitButton: 'THANKS!'})
+        }
+        else this.setState({formSent: false})
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render(){
     return (
-      <section className="about">
-        <p>BananaPhone is a Banana shaped bluetooth enabled handset that wirelessly connects to your smartphone so you can talk your friends ON A BANANA!</p>
+      <section className="container about">
+        <p><strong>BananaPhone</strong> is a banana shaped bluetooth enabled handset that wirelessly connects to your smartphone so you can talk your friends ON A BANANA!</p>
+        <form onSubmit={this.handleSubmit}>
+          <p>Sign up to stay updated on everything <strong>BananaPhone</strong></p>
+          <div className="row">
+            <div className="five columns">
+              <input type="text"
+                    name="name"
+                    placeholder="Name"
+                    className="u-full-width"
+                    disabled={this.state.formSent}
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                    onBlur={this.handleBlur} />
+              { this.state.reqName ? <p className="helper">Yellow!!? I think you forgot something... </p> : null }
+            </div>
+
+            <div className="five columns">
+              <input type="text"
+                    name="email"
+                    placeholder="Email"
+                    className="u-full-width"
+                    disabled={this.state.formSent}
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                    onBlur={this.handleBlur} />
+              { this.state.reqEmail ? <p className="helper">Yellow!!? I think you forgot something... </p> : null }
+              { this.state.invalidEmail ? <p className="helper">Double-check your email address, you might have a typo... </p> : null }
+            </div>
+
+            <div className="two columns">
+              <input type="submit"
+                    className="button"
+                    value={this.state.submitButton}
+                    disabled={this.state.formSent} />
+            </div>
+
+          </div>
+          { this.state.formSent ? <p className="helper">Thanks for signing up!</p> : null }
+        </form>
       </section>
     )
   }
--- server.js
+++ server.js
@@ -2,14 +2,52 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+const request = require('request');
+const fs = require('fs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'dist')));
+app.set('views', path.join(__dirname, 'dist'));
+
+app.post('/signup', (req, res) => {
+  const subscriberObj = req.body
+  subscriberObj.type = 'active'
+
+  const options = {
+    uri: 'https://api.mailerlite.com/api/v2/groups/6050495/subscribers',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-MailerLite-ApiKey': 'a4228ab3b0c0d43acd5ff2a4799031e8',
+    },
+    form: subscriberObj
+  }
+
+  request(options, (err, res, body) => {
+    if (err) {
+      const message = `${datetime}: ${err} \n`
+      fs.writeFileSync('error.txt', message, {flag: 'a+'})
+    }
+
+    if (!err && res.statusCode == 200) {
+      const response = JSON.parse(body)
+      const message = `${response['date_created']}: name: ${response['name']} email: ${response['email']} \n`
+      fs.writeFileSync('success.txt', message, {flag: 'a+'})
+    }
+  })
+  res.json({success:true})
+})
 
 app.all('*', (req, res, next) => {
     res.sendFile('index.html', {
@@ -18,14 +56,14 @@ app.all('*', (req, res, next) => {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
