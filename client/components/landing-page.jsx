2d89609ca6fbc266befb9419596ec579ef6cab60
Blake Netzeband <blake.netzeband@gmail.com>
2017-03-19 19:04:56 -0600

    updated fonts

--- client/components/landing-page.jsx
+++ client/components/landing-page.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class LandingPage extends Component {
+  createMarkup(title) {
+    const newTitle = title.split(' ').map((word) => {
+      return word.split('').map((letter, i) => {
+        return letter = i == 0 ? '<span class="taller">' + letter + '</span>' : letter;
+      })
+    })
+
+    return {__html: newTitle.join(' ').replace(/,/g, '')}
+  }
+
   render(){
+    const title = 'A PHONE WITH APPEAL'
     return (
       <section className="lp">
         <img src="images/logo.png" />
-        <h1>A Phone With Appeal</h1>
+        <h1 dangerouslySetInnerHTML={this.createMarkup(title)}/>
       </section>
     )
   }
