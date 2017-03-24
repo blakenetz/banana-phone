6b3d92f2e41ff460788a80bc4adf6da7ec588041
Blake Netzeband <blake.netzeband@gmail.com>
2017-03-24 14:21:37 -0600

    typo

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -4,7 +4,7 @@ class About extends Component {
   render(){
     return (
       <section className="container about">
-        <p><strong>Banana Phone</strong> is a banana shaped, bluetooth enabled mobile handset that connects with your smartphone allowing interaction with your voice assistant (siri/ Ok Google) so you can talk to your friends ON A BANANA.  We’re taking a bit out of the Apple accessory ecosystem one Banana at a time.</p>
+        <p><strong>Banana Phone</strong> is a banana shaped, bluetooth enabled mobile handset that connects with your smartphone allowing interaction with your voice assistant (siri/ Ok Google) so you can talk to your friends ON A BANANA.  We’re taking a bite out of the Apple accessory ecosystem one Banana at a time.</p>
       </section>
     )
   }
