ec0ad6ff902b1110ab879c95fe2e0b6ca60f2045
Blake Netzeband <blake.netzeband@gmail.com>
2017-03-28 18:43:20 -0600

    built for prod. added footer

--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import LandingPage from './components/landing-page';
 import Gorillas from './components/gorillas';
 import SignupForm from './components/signup-form';
+import Footer from './components/footer';
 
 require('./style/style.scss');
 
@@ -14,6 +15,7 @@ class App extends Component {
         <LandingPage />
         <Gorillas />
         <SignupForm />
+        <Footer />
       </div>
     );
   }
