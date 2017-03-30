4ec3e065fbbbeb7211f54872b661741c77381cd7
Blake Netzeband <blake.netzeband@gmail.com>
2017-03-30 08:23:47 -0600

    built for dev. remove twitter

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -6,7 +6,6 @@ class Footer extends Component {
       <footer>
         <section>
           <a href="https://www.facebook.com/bananaphone.io/" target="_blank"><i className="fa fa-facebook-square fa-3x" aria-hidden="true"></i></a>
-          <a href="" target="_blank"><i className="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>
           <a href="https://www.instagram.com/bananaphone.io/?hl=en" target="_blank"><i className="fa fa-instagram fa-3x" aria-hidden="true"></i></a>
         </section>
         <section>
