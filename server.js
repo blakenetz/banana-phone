b4f10aee6baf547531eb6c3f856ec113178c1f44
Blake Netzeband <blake.netzeband@gmail.com>
2017-05-18 22:04:42 -0600

    another cred fix. built

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ require('dotenv').config();
 const app = express();
 
 if (process.env.ENVIRONMENT === 'DEV') aws.config.loadFromPath('./AwsConfig.json');
+else {
+  aws.config.update({
+    "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
+    "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
+    "region": process.env.AWS_REGION,
+  })
+}
 const s3 = new aws.S3();
 
 app.use(logger('dev'));
