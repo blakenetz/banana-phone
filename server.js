f768415434bcbab779d266a2abddc615e4693ae3
Blake Netzeband <blake.netzeband@gmail.com>
2017-05-18 22:29:43 -0600

    removed troublesome code. built

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ require('dotenv').config();
 
 const app = express();
 
-aws.config.loadFromPath('./AwsConfig.json');
+// aws.config.loadFromPath('./AwsConfig.json');
+aws.config.update({
+  "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
+  "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
+  "region": process.env.AWS_REGION,
+})
 const s3 = new aws.S3();
 
 app.use(logger('dev'));
