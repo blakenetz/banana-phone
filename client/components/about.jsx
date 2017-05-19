9cb1b4ce14e86a498331e4c16ac815f34d4eddde
Blake Netzeband <blake.netzeband@gmail.com>
2017-05-18 22:25:39 -0600

    built

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -6,7 +6,7 @@ class About extends Component {
       <section className="container about">
         <p><strong>Banana Phone</strong> is a banana-shaped, Bluetooth<sup>&reg;</sup>-enabled, mobile handset that connects to your smartphone: interact with your voice assistant (Siri & Google Assistant) and talk to your friends <i>on a banana!</i></p>
         <p>We’re taking a bite out of the Apple accessory ecosystem one Banana at a time.</p>
-      </section>
+      </section> 
     )
   }
 
