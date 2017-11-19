a6f8a0e5ec1e92b803acf7e35b989a61326f3545
netzebandito <blake.netzeband@gmail.com>
2017-11-19 10:42:48 -0700

    updated nav bar

--- client/components/landing-page.jsx
+++ client/components/landing-page.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 
 class LandingPage extends Component {
-  render(){
-    return (
-      <section className="lp">
-        <img src="images/logo.png" alt="Banana Phone logo" />
-        
-        <h1>A PHONE WITH APPEAL</h1>
-        
-        <div className='scroll'>
-          <i className="fa fa-long-arrow-down fa-2x" aria-hidden="true"></i>
-          <p>scroll down</p>
-        </div>
-      </section>
-    )
-  }
+	render(){
+		return (
+			<section className="lp">
+				<div>
+					<h1>A PHONE WITH APPEAL</h1>
+					<img src="images/side-right.png" alt="image of a Banana Phone" />
+				</div>
+				
+				<div className='scroll'>
+					<i className="fa fa-long-arrow-down fa-2x" aria-hidden="true"></i>
+					<p>scroll down</p>
+				</div>
+			</section>
+		)
+	}
 
 }
 
