b9a721b0fbf09699354527542cf2d7889927ef8f
netzebandito <blake.netzeband@gmail.com>
2017-11-20 20:05:20 -0700

    added indiegogo badge. no longer creating indiegogo svg. added scrollreveal

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
-
-import Indiegogo from './indiegogo';
+import ScrollReveal from 'scrollreveal';
 
 export default class About extends Component {
+  componentDidMount() {
+    window.sr = ScrollReveal();
+    sr.reveal('.aboutText', { 
+      duration: 1500,
+      delay: 500,
+      reset: true,
+
+    });
+  }
+
   render(){
     return (
       <section className="container about">
-      	<div>
+      	<div className="aboutText">
 	        <p><strong>Banana Phone</strong> is a banana-shaped, Bluetooth<sup>&reg;</sup>-enabled, mobile handset that connects to your smartphone: interact with your voice assistant (Siri & Google Assistant) and talk to your friends <i>on a banana!</i></p>
 	        <p>We’re taking a bite out of the Apple accessory ecosystem one Banana at a time.</p>
-      	</div>
-      	<div>
-      		<Indiegogo />
       	</div>
       </section>
     )
--- client/components/landing-page.jsx
+++ client/components/landing-page.jsx
@@ -7,6 +7,7 @@ export default class LandingPage extends Component {
 				<div id="top">
 					<img src="images/front.png" alt="Banana Phone front view" />
 				</div>
+				<img id="igBadge" src="images/indiegogo-badge.png" alt="Successfully funded with Indiegogo" />
 			</section>
 		)
 	}
