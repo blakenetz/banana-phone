daa9a7ee05d90bb73ff5706f0488eaac10089144
netzebandito <blake.netzeband@gmail.com>
2017-11-27 18:41:05 -0700

    added more breakpoints for about section

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -9,65 +9,66 @@ export default class About extends Component {
 	        <p>We’re taking a bite out of the Apple accessory ecosystem one Banana at a time.</p>
       	</div>
 
-        <div className="stepsWrapper"> 
+        <div className="stepsWrapper">
 
-        <div className="steps">
-          <div className="iconWrap">
-            <i className="fa fa-mobile" aria-hidden="true"></i>
+          <div className="steps">
+            <div className="iconWrap">
+              <i className="fa fa-mobile" aria-hidden="true"></i>
+            </div>
+            <div className="iconWrap">
+              <i className="fa fa-plus bp-plus" aria-hidden="true"></i>
+            </div>
+            <div className="iconWrap">
+              <img src="images/logo-blacktext.png" alt="Banana Phone logo" />
+            </div>
+            <div className="instruction">
+              <p><strong>Step One</strong></p>
+              <ul>
+                <li>Pair your smartphone via Bluetooth</li>
+              </ul>
+            </div>
           </div>
-          <div className="iconWrap">
-            <i className="fa fa-plus bp-plus" aria-hidden="true"></i>
-          </div>
-          <div className="iconWrap">
-            <img src="images/logo-blacktext.png" alt="Banana Phone logo" />
-          </div>
-          <div className="instruction">
-            <p><strong>Step One</strong></p>
-            <ul>
-              <li>Pair your smartphone via Bluetooth</li>
-            </ul>
-          </div>
-        </div>
 
-        <div className="steps">
-          <div className="iconWrap">
-            <i className="fa fa-phone" aria-hidden="true"></i>
-            <i className="fa fa-arrow-down bp-arrow" aria-hidden="true"></i>
+          <div className="steps">
+            <div className="iconWrap">
+              <i className="fa fa-phone" aria-hidden="true"></i>
+              <i className="fa fa-arrow-down bp-arrow" aria-hidden="true"></i>
+            </div>
+            <div className="iconWrap">
+              <i className="fa fa-plus bp-plus" aria-hidden="true"></i>
+            </div>
+            <div className="iconWrap">
+              <img src="images/logo-blacktext.png" alt="Banana Phone logo" />
+            </div>
+            <div className="instruction">
+              <p><strong>Step Two</strong></p>
+              <ul>
+                <li>Recieve calls on your Banana Phone</li>
+                <li>Click the multifunctional button to answer</li>
+              </ul>
+            </div>
           </div>
-          <div className="iconWrap">
-            <i className="fa fa-plus bp-plus" aria-hidden="true"></i>
-          </div>
-          <div className="iconWrap">
-            <img src="images/logo-blacktext.png" alt="Banana Phone logo" />
-          </div>
-          <div className="instruction">
-            <p><strong>Step Two</strong></p>
-            <ul>
-              <li>Recieve calls on your Banana Phone</li>
-              <li>Click the multifunctional button to answer</li>
-            </ul>
-          </div>
-        </div>
 
-        <div className="steps">
-          <div className="iconWrap">
-            <i className="fa fa-phone" aria-hidden="true"></i>
-            <i className="fa fa-arrow-up bp-arrow" aria-hidden="true"></i>
+          <div className="steps">
+            <div className="iconWrap">
+              <i className="fa fa-phone" aria-hidden="true"></i>
+              <i className="fa fa-arrow-up bp-arrow" aria-hidden="true"></i>
+            </div>
+            <div className="iconWrap">
+              <i className="fa fa-plus bp-plus" aria-hidden="true"></i>
+            </div>
+            <div className="iconWrap">
+              <img src="images/logo-blacktext.png" alt="Banana Phone logo" />
+            </div>
+            <div className="instruction">
+              <p><strong>Step Three</strong></p>
+              <ul>
+                <li>Initiate calls from your Banana Phone</li>
+                <li>Voice activate calling with Siri and OK Google</li>
+              </ul>
+            </div>
           </div>
-          <div className="iconWrap">
-            <i className="fa fa-plus bp-plus" aria-hidden="true"></i>
-          </div>
-          <div className="iconWrap">
-            <img src="images/logo-blacktext.png" alt="Banana Phone logo" />
-          </div>
-          <div className="instruction">
-            <p><strong>Step Three</strong></p>
-            <ul>
-              <li>Initiate calls from your Banana Phone</li>
-              <li>Voice activate calling with Siri and OK Google</li>
-            </ul>
-          </div>
-        </div>
+
         </div>
       </section>
     )
