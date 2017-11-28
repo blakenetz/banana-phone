03932b144815f10824d2e8773290fc3a99f71f3f
blakeface <blake.netzeband@gmail.com>
2017-11-28 16:47:40 -0700

    updated landing section to include parallax

--- client/components/landing-page.jsx
+++ client/components/landing-page.jsx
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 
 export default class LandingPage extends Component {
-  render(){
-    return (
-      <section className="lp">
-        <img src="images/logo.png" alt="Banana Phone logo" />
-        <h1>A PHONE WITH APPEAL</h1>
-        <div className='scroll'>
-          <i className="fa fa-long-arrow-down fa-2x" aria-hidden="true"></i>
-          <p>scroll down</p>
-        </div>
-      </section>
-    )
-  }
+	render(){
+		return (
+			<section className="landingPage">
+				<div className="lp">
+					<div className="logoWrapper">
+						<img src="images/logo.png" alt="Banana Phone logo" />
+						<h1>A PHONE WITH APPEAL</h1>
+					</div>
+
+					<div className='scroll'>
+						<i className="fa fa-long-arrow-down fa-2x" aria-hidden="true"></i>
+						<p>scroll down</p>
+					</div>
+
+				</div>
+
+				<div className="phone">
+					<img src="/images/phone-tilted.png" alt="upclose image of the Banana Phone" />
+				</div>
+
+			</section>
+		)
+	}
 }
